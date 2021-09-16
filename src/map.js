7886d684e090c06859bed990fa7e83e9f92b13f5
Matthew Mone <mmone@vernier.com>
2021-09-16 14:10:41 -0700

    map to off main

---
+++ src/map.js
@@ -0,0 +1,60 @@
+import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js';
+
+const canvas = document.querySelector('#map');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+if (!canvas.transferControlToOffscreen) {
+  canvas.style.display = 'none';
+  document.querySelector('#noOffscreenCanvas').style.display = '';
+}
+const offscreen = canvas.transferControlToOffscreen();
+const worker = new Worker('./src/scene.js', {
+  type: 'module',
+});
+
+worker.postMessage({ type: 'main', canvas: offscreen }, [offscreen]);
+
+function sendSize() {
+  worker.postMessage({
+    type: 'size',
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+}
+
+// const controls = new OrbitControls(camera, canvas);
+// controls.maxZoom = 2;
+canvas.addEventListener('dragstart', event => {
+  event.dataTransfer.setDragImage(new Image(0, 0), 0, 0);
+  worker.postMessage({ type: 'dragStart', x: event.x, y: event.y });
+});
+canvas.addEventListener('drag', event => {
+  event.preventDefault();
+  worker.postMessage({ type: 'drag', x: event.x, y: event.y });
+});
+document.addEventListener('dragover', event => {
+  event.preventDefault();
+});
+canvas.addEventListener(
+  'mousewheel',
+  event => {
+    worker.postMessage({ type: 'zoom', delta: -event.deltaY });
+  },
+  { passive: true },
+);
+
+// canvas.addEventListener('click', () => {
+//   worker.postMessage({
+//     type: 'toggleRotation',
+//   });
+// });
+
+document.querySelector('button').addEventListener('click', () => {
+  worker.postMessage({
+    type: 'newMap',
+  });
+});
+
+window.addEventListener('resize', sendSize);
+sendSize();
--- src/scene.js
+++ src/scene.js
@@ -12,8 +12,8 @@ import {
   Vector2,
   Vector3,
   CanvasTexture,
-} from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+} from 'https://cdn.skypack.dev/three';
+
 import { makeMap } from './mapping.js';
 import { createTerrainSide } from './canvasTextures.js';
 import { rollDice } from './utils/rollDice.js';
@@ -31,96 +31,108 @@ const mapTypes = [
 ];
 // const mapTypes = ['mountain'];
 
+const state = {
+  width: 300,
+  height: 150,
+  rotate: true,
+  zoom: 1,
+};
+
 const raycaster = new Raycaster();
 const mouse = new Vector2();
+let camera, scene;
+
 let intersectionObject;
-const scene = new Scene();
-const aspect = window.innerWidth / window.innerHeight;
-const d = 20;
-const camera = new OrthographicCamera(-d * aspect, d * aspect, d, -d, 1, 1000);
+function main({ canvas }) {
+  scene = new Scene();
+  const aspect = canvas.width / canvas.height;
+  const d = 20;
+  camera = new OrthographicCamera(-d * aspect, d * aspect, d, -d, 1, 1000);
 
-const light = new AmbientLight(0xffffff, 0.8);
-scene.add(light);
+  const light = new AmbientLight(0xffffff, 0.8);
+  scene.add(light);
 
-const group = renderMap();
-scene.add(group);
+  const group = renderMap();
+  scene.add(group);
 
-const focalPoint = new Vector3(4, 4, 10);
-camera.position.set(focalPoint.x + 15, focalPoint.y + 15, focalPoint.z + 15);
-camera.lookAt(focalPoint);
-camera.updateProjectionMatrix();
+  const focalPoint = new Vector3(0, 5, 0);
+  camera.position.set(focalPoint.x + 15, focalPoint.y + 15, focalPoint.z + 15);
+  camera.lookAt(focalPoint);
+  camera.updateProjectionMatrix();
 
-const directionalLight = new DirectionalLight(0xffffff, 1);
-directionalLight.position.set(-30, 30, 30);
-directionalLight.target = group;
+  const directionalLight = new DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(-30, 30, 30);
+  directionalLight.target = group;
 
-scene.add(directionalLight);
+  scene.add(directionalLight);
 
-const renderer = new WebGLRenderer();
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.maxZoom = 2;
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-document.querySelector('button').addEventListener('click', () => {
+  const renderer = new WebGLRenderer({ canvas });
+  renderer.setSize(canvas.width, canvas.height, false);
+  // renderer.domElement.addEventListener('click', event => {
+  //   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  //   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  //   raycaster.setFromCamera(mouse, camera);
+
+  //   const intersects = raycaster.intersectObject(scene, true);
+
+  //   if (intersects.length) {
+  //     const [
+  //       {
+  //         object: { position },
+  //       },
+  //     ] = intersects;
+  //     const { x, y, z } = position;
+  //     console.log(x, y, z);
+  //     let xModifier = 1;
+  //     let zModifier = 1;
+  //     if (x < 6) xModifier = -1;
+  //     if (z < 12) zModifier = -1;
+  //     camera.position.set(x + 10 * xModifier, y + 10, z + 10 * zModifier);
+  //     camera.lookAt(position);
+  //   }
+  // });
+
+  // renderer.domElement.addEventListener('mousemove', event => {
+  //   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  //   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  //   raycaster.setFromCamera(mouse, camera);
+
+  //   const intersects = raycaster.intersectObject(scene, true);
+
+  //   if (intersects.length) {
+  //     if (intersects[0].object !== intersectionObject) {
+  //       if (intersectionObject) {
+  //         intersectionObject.material.color.set(intersectionObject.priorColor);
+  //       }
+  //       intersectionObject = intersects[0].object;
+  //       intersectionObject.priorColor = JSON.parse(JSON.stringify(intersectionObject.material.color));
+  //       intersectionObject.material.color.set(intersectionObject.material.color.addScalar(0.5));
+  //     }
+  //   } else if (intersectionObject)
+  //     intersectionObject.material.color.set(intersectionObject.priorColor);
+  // });
+
+  function animate(time) {
+    const rotation = (state.delta?.x || 0) / 50;
+    scene.rotation.y = rotation;
+    scene.rotation.x = Math.sin((-10 * Math.PI) / 180);
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+}
+
+function newMap() {
   const oldMap = scene.getObjectByName('map');
   scene.remove(oldMap);
   const mapGroup = renderMap();
   scene.add(mapGroup);
-});
-// renderer.domElement.addEventListener('click', event => {
-//   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-//   raycaster.setFromCamera(mouse, camera);
-
-//   const intersects = raycaster.intersectObject(scene, true);
-
-//   if (intersects.length) {
-//     const [
-//       {
-//         object: { position },
-//       },
-//     ] = intersects;
-//     const { x, y, z } = position;
-//     console.log(x, y, z);
-//     let xModifier = 1;
-//     let zModifier = 1;
-//     if (x < 6) xModifier = -1;
-//     if (z < 12) zModifier = -1;
-//     camera.position.set(x + 10 * xModifier, y + 10, z + 10 * zModifier);
-//     camera.lookAt(position);
-//   }
-// });
-
-// renderer.domElement.addEventListener('mousemove', event => {
-//   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-//   raycaster.setFromCamera(mouse, camera);
-
-//   const intersects = raycaster.intersectObject(scene, true);
-
-//   if (intersects.length) {
-//     if (intersects[0].object !== intersectionObject) {
-//       if (intersectionObject) {
-//         intersectionObject.material.color.set(intersectionObject.priorColor);
-//       }
-//       intersectionObject = intersects[0].object;
-//       intersectionObject.priorColor = JSON.parse(JSON.stringify(intersectionObject.material.color));
-//       intersectionObject.material.color.set(intersectionObject.material.color.addScalar(0.5));
-//     }
-//   } else if (intersectionObject)
-//     intersectionObject.material.color.set(intersectionObject.priorColor);
-// });
-
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
 }
-
-animate();
 
 function renderMap() {
   const type = oneOf(mapTypes);
-  document.querySelector('#mapType').textContent = type;
+  // document.querySelector('#mapType').textContent = type;
   const map = makeMap({ type });
 
   const boxGeometry = new BoxGeometry();
@@ -200,3 +212,47 @@ function renderMap() {
   group.name = 'map';
   return group;
 }
+
+function size(data) {
+  state.width = data.width;
+  state.height = data.height;
+}
+
+function dragStart({ x, y }) {
+  state.start = { x, y };
+  if (state.delta) {
+    state.start.x -= state.delta.x;
+    state.start.y -= state.delta.y;
+  }
+}
+
+function drag({ x, y }) {
+  state.delta = { x: x - state.start.x, y: y - state.start.y };
+}
+
+function zoom({ delta }) {
+  camera.zoom = Math.min(4, Math.max(0.5, camera.zoom + delta / 700));
+  camera.updateProjectionMatrix();
+}
+
+function toggleRotation() {
+  state.rotate = !state.rotate;
+  delete state.start;
+}
+
+const handlers = {
+  main,
+  size,
+  newMap,
+  toggleRotation,
+  dragStart,
+  drag,
+  zoom,
+};
+self.onmessage = function (e) {
+  const fn = handlers[e.data.type];
+  if (!fn) {
+    throw new Error('no handler for type: ' + e.data.type);
+  }
+  fn(e.data);
+};
