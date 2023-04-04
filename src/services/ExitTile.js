8f24292704728246cbbd91102876879bbc638d14
mMatthew Mone <mattmone@gmail.com>
2023-04-04 16:34:33 +0000

    refactor(ExitTile.js): add constructor parameter for exitTileDetails and set direction property in constructor feat(ExitTile.js): add getters for northSouth and fromDirection properties docs(ExitTile.js): add JSDoc comments for constructor and typedef for ExitTileDetails

--- src/services/ExitTile.js
+++ src/services/ExitTile.js
@@ -1,11 +1,29 @@
-import { IdbBacked } from "../utils/baseClasses/IdbBacked.js";
+import { DIRECTION, OPPOSITE_DIRECTION } from "../constants/directions.js";
 import { FloorTile } from "./FloorTile.js";
 
 export class ExitTile extends FloorTile {
-  constructor(id = crypto.randomUUID(), props) {
-    super(id, props);
+  /**
+   * creates the exitTile
+   * @param {string} id the id of the exitTile
+   * @param {ExitTileDetails} exitTileDetails the details of the exitTile
+   */
+  constructor(id = crypto.randomUUID(), exitTileDetails) {
+    super(id, exitTileDetails);
+    this.direction = exitTileDetails.direction;
   }
   get isExit() {
     return true;
   }
+
+  get northSouth() {
+    return [DIRECTION.NORTH, DIRECTION.SOUTH].includes(this.direction);
+  }
+
+  get fromDirection() {
+    return OPPOSITE_DIRECTION[this.direction];
+  }
 }
+
+/**
+ * @typedef {import('../../types').ExitTileDetails} ExitTileDetails
+ */
