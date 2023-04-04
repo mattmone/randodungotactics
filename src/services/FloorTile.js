f6d5644c141e6fd07c9b8078c523255e3ab14ee0
mMatthew Mone <mattmone@gmail.com>
2023-04-04 16:34:46 +0000

    refactor(FloorTile.js): change constructor to use TileDetails object instead of individual parameters feat(FloorTile.js): add new properties to FloorTile class: northWall, southWall, eastWall, westWall feat(FloorTile.js): add new methods to FloorTile class: position, hasWall, northSouthWall, eastWestWall, at()

--- src/services/FloorTile.js
+++ src/services/FloorTile.js
@@ -1,17 +1,30 @@
 import { IdbBacked } from "../utils/baseClasses/IdbBacked";
 
 export class FloorTile extends IdbBacked {
-  constructor(id = crypto.randomUUID(), {x, z, terrain = 'rock'}) {
+  /**
+   * 
+   * @param {string} id the id of the floorTile
+   * @param {TileDetails} tileDetails the details of the floorTile
+   */
+  constructor(id = crypto.randomUUID(), tileDetails) {
     super(id);
-    this.x = x;
-    this.z = z;
-    this.terrain = terrain;
+    this.x = tileDetails.x;
+    this.z = tileDetails.z;
+    this.northWall = tileDetails.northWall;
+    this.southWall = tileDetails.southWall;
+    this.eastWall = tileDetails.eastWall;
+    this.westWall = tileDetails.westWall;
+    this.terrain = tileDetails.terrain ?? 'rock';
   }
 
   static get serialized() {
     return {
         x: true,
         z: true,
+        northWall: true,
+        southWall: true,
+        eastWall: true,
+        westWall: true,
         terrain: true
     };
   }
@@ -20,7 +33,33 @@ export class FloorTile extends IdbBacked {
     return `${this.x}|${this.z}`;
   }
 
+  get position() {
+    return {x: this.x, z: this.z};
+  }
+
+  get hasWall() {
+    return this.northWall || this.southWall || this.eastWall || this.westWall;
+  }
+
+  get northSouthWall() {
+    return this.northWall || this.southWall;
+  }
+
+  get eastWestWall() {
+    return this.eastWall || this.westWall;
+  }
+
+  /**
+   * find out if the floorTile is at the given position
+   * @param {number} x the x position
+   * @param {number} z the z position
+   * @returns {boolean} true if the floorTile is at the given position
+   */
   at(x, z) {
     return x === this.x && z === this.z;
   }
 }
+
+/**
+ * @typedef {import('../../types').TileDetails} TileDetails
+ */
